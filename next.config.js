c5d9a78d8d592cef8298835c2e72c1df6043ded6
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-02-03 12:16:59 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  env: {
+    NEXT_PUBLIC_RPC: process.env.NEXT_PUBLIC_RPC,
+  },
+};
